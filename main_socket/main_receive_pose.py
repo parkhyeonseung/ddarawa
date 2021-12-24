35950b363103d8d6188fce00feb7d470fa3cd218
parkheonseung <gus0tmd@naver.com>
2021-12-24 18:08:00 +0900

    1

--- main_socket/main_receive_pose.py
+++ main_socket/main_receive_pose.py
@@ -4,15 +4,13 @@ import rospy
 import socket
 import pickle
 
-master_ip = '192.168.0.16'
-
-def func():
-    pass
-
 if __name__=='__main__':
+    rospy.init_node('master_receive_pose')
+    main_ip = '192.168.0.16'
     receiver = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    receiver.bind((master_ip,7778))
+    receiver.bind((main_ip,7778))
+    
     while True:
         bytepair = receiver.recvfrom(1024)
         message = pickle.loads(bytepair[0]) 
-        print(message)
+        
--- turtle1_socket/turtle_pose_pub.py
+++ turtle1_socket/turtle_pose_pub.py
@@ -5,15 +5,14 @@ import socket
 from geometry_msgs.msg import PoseWithCovarianceStamped
 import pickle
 
-master_ip = '192.168.0.16'
-
-
 def callback(data):
     pose_x = data.pose.pose.position.x
     pose_y = data.pose.pose.position.y
     pose_z = data.pose.pose.orientation.z
+    pose_w = data.pose.pose.orientation.w
     print('data import')
-    position = [pose_x,pose_y,pose_z]
+    
+    position = [pose_x,pose_y,pose_z,pose_w]
     position=pickle.dumps(position)
 
     sender = socket.socket(family=socket.AF_INET,type = socket.SOCK_DGRAM)
@@ -21,7 +20,8 @@ def callback(data):
 
 
 if __name__ =='__main__':
-    rospy.init_node('pose_pub')
+    master_ip = '192.168.0.16'
+    rospy.init_node('turtle_pose_pub')
     rospy.Subscriber('/amcl_pose',PoseWithCovarianceStamped,callback=callback)
     rospy.spin()
     
