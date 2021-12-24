3013665f2f4a42348769f8fa2bec315c2003fc20
ghj1 <ghj12222@gmail.com>
2021-12-24 18:45:45 +0900

    M_sub_socket_T

---
+++ main_socket/M_sub_socket_T5.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+import rospy
+import socket
+import pickle
+
+def callback_t5(data):
+    t5_ip = '192.168.0.16'
+    sender = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    data = pickle.dumps(data)
+    sender.sendto(data,(t5_ip,7778))
+
+
+if __name__=='__main__':
+    rospy.init_node('master_receive_pose')
+    rospy.Subscriber('t5_command', callback=callback_t5)
+    rospy.spin()
+
+    pass
