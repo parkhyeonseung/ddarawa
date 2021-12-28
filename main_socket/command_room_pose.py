7e5309d6266c71576bf57c18d0691167942c1597
parkheonseung <gus0tmd@naver.com>
2021-12-28 17:47:15 +0900

    room socket

---
+++ main_socket/command_room_pose.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python3
+
+import rospy
+import socket
+import pickle
+import rospy
+import sys, select, termios, tty
+
+def getKey():
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+
+    if rlist:
+        key = sys.stdin.read(1)
+    else:
+        key = ''
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
+
+if __name__ == '__main__':
+    rospy.init_node('movebase_client_py')
+
+    turtle1_ip = '192.168.0.31'
+    turtle2_ip = '192.168.0.31'
+    turtle3_ip = '192.168.0.31'
+    turtle4_ip = '192.168.0.31'
+
+    sender = socket.socket(family=socket.AF_INET,type = socket.SOCK_DGRAM)
+
+    settings = termios.tcgetattr(sys.stdin)
+    prev_pos = None
+    tar_position=None
+    bot_ip = None
+    room_1 = [1, 1.0, -0.5, 1.0]  ## room number , x, y, z
+    room_2 = [2, 2.0, 2.0, 2.0]
+    room_3 = [3, 3.0, 3.0, 3.0]
+    room_4 = [4, 4.0, 4.0, 4.0]
+
+    while True:
+        try:
+            key = getKey()
+            if key == '1':
+                print('room 1')
+                tar_position = room_1
+                bot_ip = turtle1_ip
+            elif key == '2':
+                tar_position = room_2
+                bot_ip = turtle2_ip
+            elif key == '3':
+                tar_position = room_3
+                bot_ip = turtle2_ip
+            elif key == '4':
+                tar_position = room_4
+                bot_ip = turtle2_ip
+
+            elif key == 'q':
+                break
+            else :
+                tar_position = None
+
+            if tar_position != None:
+                room_pose=pickle.dumps(tar_position)
+                sender.sendto(room_pose,(bot_ip,5555))
+
+        except KeyboardInterrupt :
+            break
---
+++ turtle2_socket/turtle2_room_sub.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+import rospy
+import socket
+import pickle
+from geometry_msgs.msg import PoseStamped
+
+pub = rospy.Publisher('/move_base_simple/goal',PoseStamped,queue_size=10)
+
+turtle2_ip = '192.168.0.16'
+receiver = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+receiver.bind((turtle2_ip,5555))
+
+while True:
+    bytepair = receiver.recvfrom(1024)
+    room_data = pickle.loads(bytepair[0]) 
+    goal = PoseStamped()
+    goal.header.frame_id= 'map'
+    goal.pose.orientation.w =float(0.5)
+    goal.pose.position.x = float(room_data[1])
+    goal.pose.position.y  = float(room_data[2])
+    goal.pose.orientation.z  = float(room_data[3])
+    rospy.sleep(0.5)
+    pub.publish(goal)
