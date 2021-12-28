e35e5c55a1d993a8c0c03b249cd696a54576787e
parkheonseung <gus0tmd@naver.com>
2021-12-28 17:33:12 +0900

    socket

--- turtle5_socket/turtle5_pose_pub.py
+++ turtle5_socket/turtle5_pose_pub.py
@@ -12,7 +12,7 @@ def callback(data):
     pose_w = data.pose.pose.orientation.w
     print('data import')
     
-    position = ['T1',pose_x,pose_y,pose_z,pose_w]
+    position = ['T5',pose_x,pose_y,pose_z,pose_w]
     position=pickle.dumps(position)
 
     sender = socket.socket(family=socket.AF_INET,type = socket.SOCK_DGRAM)
@@ -21,7 +21,7 @@ def callback(data):
 
 if __name__ =='__main__':
     master_ip = '192.168.0.16'
-    rospy.init_node('turtle1_pose_pub')
+    rospy.init_node('turtle5_pose_pub')
     rospy.Subscriber('/amcl_pose',PoseWithCovarianceStamped,callback=callback)
     rospy.spin()
     
